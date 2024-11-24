a1db1e0972a2ba9e6eec9daa36faec6fd1cbc2f6
sandersirge <sandersirge@gmail.com>
2024-11-24 11:14:46 +0200

    Modified header and footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,12 @@
 <template>
-
+    <div class="footer">
+        <p>Copyright © 2024 Group AS</p>
+        <h4>Links to the resources of this course</h4>
+        <ul>
+            <li><a href="https://ois2.ut.ee/#/courses/LTAT.05.004/version/4db291d6-0997-e142-b9d4-9ee95cdacf20/details" target="_blank" rel="noopener">OIS</a></li>
+            <li><a href="https://courses.cs.ut.ee/2024/WAD/fall" target="_blank" rel="noopener">Courses</a></li>
+        </ul>
+    </div>
 </template>
 
 <script>
