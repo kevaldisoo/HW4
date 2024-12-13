3a4f257e91e114b6eac8529a3aaede344fc32fcf
raiomitt <raiomitt2@gmail.com>
2024-12-13 10:52:27 +0200

    Changed index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import SignUp from "../views/SignUpView.vue";
 import Login from "../views/LoginView.vue";
+import Contacts from "../views/ContactView.vue";
 
 const routes = [
   {
@@ -12,30 +13,17 @@ const routes = [
   {
     path: '/SignUp',
     name: 'signUp',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: SignUp
   },
   {
     path: '/Login',
     name: 'login',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-<<<<<<< HEAD
     component: Login
-=======
-    component: () => import(/* webpackChunkName: "about" */ '../views/LoginView.vue')
   },
   {
     path: '/Contacts',
     name: 'contacts',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/ContactView.vue')
->>>>>>> e518f769669a063d9ea84bfa97621cc9de9ed264
+    component: Contacts
   }
 ]
 
